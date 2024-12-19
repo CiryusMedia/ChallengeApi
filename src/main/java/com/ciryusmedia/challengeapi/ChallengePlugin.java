6f3593df53225da031fcd885617a06e401aef393
ImCiryus <nentonde@gmail.com>
2024-12-19 16:13:18 +0100

    Added the challenge timer

--- src/main/java/com/ciryusmedia/challengeapi/ChallengePlugin.java
+++ src/main/java/com/ciryusmedia/challengeapi/ChallengePlugin.java
@@ -1,9 +1,13 @@
 package com.ciryusmedia.challengeapi;
 
+import com.ciryusmedia.challengeapi.timer.ChallengeTimer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class ChallengePlugin extends JavaPlugin {
 
+    protected ChallengeTimer timer;
 
-
+    public ChallengeTimer getTimer() {
+        return timer;
+    }
 }
